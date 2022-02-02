9a3ef7a01c0e1c958f552b3e5cbd6e0c701206a7
장명훈 Jang MyeongHoon <98157935+indoor98@users.noreply.github.com>
2022-02-03 00:51:27 +0900

    Update leet 561. Array Partition I.py

--- leet 561. Array Partition I.py
+++ leet 561. Array Partition I.py
@@ -10,3 +10,20 @@ class Solution:
         
         return sum
             
+#enumerate 사용 (위 코드보다 조금 더 빠름)
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        sum = 0
+        nums.sort()
+        i = 0
+        for i, n in enumerate(nums):
+            if i % 2 == 0:
+                sum += n
+        return sum
+
+#파이썬다운 방식(슬라이싱 활용)
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        return sum(sorted(nums)[::2])
+
+    #슬라이싱은 굉장히 빠르고 간결하다.
