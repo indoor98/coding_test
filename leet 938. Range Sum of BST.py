1d9adefb6ed2ddd966986aeaf9f36cd8ec89f3eb
Jang MyeongHoon <98157935+indoor98@users.noreply.github.com>
2022-02-22 17:04:20 +0900

    Create leet 938. Range Sum of BST.py

---
+++ leet 938. Range Sum of BST.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+#DFS를 이용한 브루트포스 풀이
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if not root:
+            return 0
+        return (root.val if low <= root.val <= high else 0) + \
+               (self.rangeSumBST(root.left, low, high)) +\
+               (self.rangeSumBST(root.right, low, high))
+      
+#가지치기
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        def dfs(node:ListNode):
+            if not node:
+                return 0
+        
+            if node.val < low:
+                return dfs(node.right)
+            elif node.val > high:
+                return dfs(node.left)
+            return node.val + dfs(node.left) + dfs(node.right)
+        
+        return dfs(root)
+      
+# 반복구조 DFS로 필요한 노드 탐색
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        stack, sum = [root], 0
+        while stack:
+            node = stack.pop()
+            if node:
+                if node.val > low:
+                    stack.append(node.left)
+                if node.val < high:
+                    stack.append(node.right)
+                if low <= node.val <= high :
+                    sum += node.val
+            
+        return sum
+            
